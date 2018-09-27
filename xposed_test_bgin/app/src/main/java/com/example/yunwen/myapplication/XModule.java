89706fdb758f5c536ceb6bb65ebc5d6ae8beed21
AlvinLei <yunwenl@gmail.com>
2018-09-27 09:59:48 -0700

    initial commit

--- xposed_test_bgin/app/src/main/java/com/example/yunwen/myapplication/XModule.java
+++ xposed_test_bgin/app/src/main/java/com/example/yunwen/myapplication/XModule.java
@@ -1,19 +1,32 @@
 package com.example.yunwen.myapplication;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
 /**
  * Created by yunwe on 9/25/2018.
  */
-
 public class XModule implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         XposedBridge.log("load app:"+lpparam.packageName);
-        if (lpparam.packageName.equals("com.example.yunwen.myapplication")) {
-            XposedBridge.log("...........find it............");
-        }
+        if (!lpparam.packageName.equals("com.android.systemui"))
+            return;
+
+        findAndHookMethod("com.android.systemui.statusbar.policy.Clock", lpparam.classLoader, "updateClock", new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                // this will be called before the clock was updated by the original method
+            }
+            @Override
+            protected void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                // this will be called after the clock was updated by the original method
+            }
+        });
+
     }
 }
